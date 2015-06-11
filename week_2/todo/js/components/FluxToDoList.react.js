45a7080ca69c8cb121c611355f893f528e80de10
Leslie Kim <leslie.kimm@gmail.com>
2015-06-11 13:58:15 -0400

    Written FluxToDoList CV

--- week_2/todo/js/components/FluxToDoList.react.js
+++ week_2/todo/js/components/FluxToDoList.react.js
@@ -5,9 +5,38 @@ var FluxToDoActions = require('../actions/FluxToDoActions');
 var FluxToDoList = React.createClass({
   // Add item to ToDo list via actions
   addToDoItem: function(event) {
-    var item = this.props.selected.item;
+    var itemID = this.props.selected.itemID;
     var update = {
-      name: this.props.toDo
+      item = this.props.selected.item;
     }
+    FluxToDoActions.addToDoItem(itemID, update);
+    FLuxToDoActions.updateToDoVisible(true);
+  },
+
+  // Select item index via actions
+  selectIndexItem: function(event) {
+    FluxToDoActions.selectToDoItem(event.target.value);
+  },
+
+  // Render To Do List View
+  render: function() {
+    return (
+      <div className="flux-toDo">
+        <div className="flux-toDo-detail">
+          <h1 className="name">{this.props.items.name}</h1>
+          <p className="description">{this.props.items.description}</p>
+          <select onChange={this.selectIndexItem}>
+            {this.props.items.variants.map(function(variant, index) {
+              return (
+                <option key={index} value={index}>{variant.type}</option>
+              )
+            })}
+          </select>
+          <button type="button" onClick={this.addToDoItem}></button>
+        </div>
+      </div>
+    );
   }
 })
+
+module.exports = FluxToDoList;
